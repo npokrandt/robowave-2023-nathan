a73b76071a620b05260bc0485f184e14ee9c5946
npokrandt <npokrandt919@gmail.com>
2023-06-27 21:13:26 -0500

    fullpage and typewriter

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,22 @@
-console.log('Hello robo!')
+$(document).ready(function(){
+    $('#fullpage').fullpage({
+        licenseKey: 'gplv3-license',
+        navigation: true,
+        navigationTooltips: ['Welcome', 'New Robots', 'Used Robots', 'Contact']
+        //options here
+    })
+
+    var typewriterElements = document.querySelectorAll('.typewriter-effect')
+    //console.log(typewriterElements)
+    for (var i = 0; i < typewriterElements.length;i++){
+        var currentEl = typewriterElements[i]
+        var innerText = currentEl.innerText
+        new Typewriter(currentEl, {
+            loop: true
+        })
+        .typeString(innerText)
+        .pauseFor(1500)
+        .start() 
+
+    }
+})
